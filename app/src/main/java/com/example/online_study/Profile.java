c0bcaab9e9cbabea937ca73e3331eb9c0010c531
sammitpal2000 <sammit.dp.pal2000@gmail.com>
2020-09-03 20:03:14 +0530

    online study

---
+++ app/src/main/java/com/example/online_study/Profile.java
@@ -0,0 +1,74 @@
+package com.example.online_study;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+public class Profile extends AppCompatActivity {
+
+    TextView uname,uemail,uphone;
+    ImageView gimage;
+    FirebaseDatabase mData;
+    DatabaseReference mRef;
+    FirebaseAuth mAuth;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        uname = (TextView) findViewById(R.id.uname);
+        uemail = (TextView) findViewById(R.id.umail);
+        uphone = (TextView) findViewById(R.id.uphone);
+        gimage = (ImageView) findViewById(R.id.gimage);
+
+        mData = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        mRef = mData.getReference(mAuth.getUid());
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+        storageReference.child(mAuth.getUid()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(gimage);
+            }
+        }) ;
+
+
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                uname.setText(userProfile.getName());
+                uemail.setText(userProfile.getEmail());
+                uphone.setText(userProfile.getPhone());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
