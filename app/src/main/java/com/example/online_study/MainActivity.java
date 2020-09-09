4db554cbfb0e9d1467fb358beab33a2512a2efcb
sammitpal2000 <sammit.dp.pal2000@gmail.com>
2020-09-09 21:02:46 +0530

    online study

--- app/src/main/java/com/example/online_study/MainActivity.java
+++ app/src/main/java/com/example/online_study/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.online_study;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,14 +19,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this,Login.class);
-                startActivity(intent);
-                finish();
-            }
-        },2000);
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            connected = true;
+        }
+        else
+            connected = false;
+
+        if(connected==true){
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this,Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },2000);
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this,"Please connect to internet and try again",Toast.LENGTH_LONG).show();
+        }
     }
 }
